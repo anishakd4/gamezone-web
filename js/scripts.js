6292ba1806a9ea1ef13f4bc3afe466eafc969a2b
Anish kumar dubey <anishkumardubey@Anishs-MacBook-Pro-9.local>
2025-07-25 08:27:06 +0530

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+
+$(function () {
+    "use strict";
+
+    function gridGallery() {
+        if ($(".gallery-items").length) {
+            var $grid = $(".gallery-items").isotope({
+                singleMode: true,
+                columnWidth: ".grid-sizer, .grid-sizer-second, .grid-sizer-three",
+                itemSelector: ".gallery-item, .gallery-item-second, .gallery-item-three"
+            });
+            $grid.imagesLoaded(function () {
+                $grid.isotope("layout");
+            });
+            $(".gallery-filters").on("click", "a.gallery-filter", function (b) {
+                b.preventDefault();
+                var c = $(this).attr("data-filter");
+                $grid.isotope({
+                    filter: c
+                });
+                $(".gallery-filters a").removeClass("gallery-filter-active");
+                $(this).addClass("gallery-filter-active");
+            });
+        }
+    }
+
+    function hoverdirInit() {
+        $(".hde .gallery-item").each(function () {
+            $(this).hoverdir();
+        });
+    }
+
+    function scrollDown() {
+        $(".scroll_down").click(function() {
+            $('html, body').animate({
+                scrollTop: $("#main").offset().top
+            }, 1500);
+        });
+    }
+    gridGallery();
+    hoverdirInit();
+    scrollDown();
+
+});
+
+
